620a0cc5bedf23a4a479eab858424a171467d56c
Shivanshu Singh <shivanshusinghraj@gmail.com>
2017-08-31 18:46:55 +0530

    Use K&R coding style

--- ClassExercises/sysfs_chardev/open.c
+++ ClassExercises/sysfs_chardev/open.c
@@ -4,15 +4,15 @@
 
 main()
 {
-   char buf[10] = {0};
-   int fd = open("/dev/demo",O_RDWR);
-   if(fd < 0){
-       perror("open");
-       exit(-1);
-   }
-   write(fd, "Xcvb\0", 10);
-   sleep(25);
-   ssize_t size = read(fd, buf, 10);
-   printf("Read: %s\n", buf);
-   close(fd);
+	char buf[10] = { 0 };
+	int fd = open("/dev/demo", O_RDWR);
+	if (fd < 0) {
+		perror("open");
+		exit(-1);
+	}
+	write(fd, "Xcvb\0", 10);
+	sleep(25);
+	ssize_t size = read(fd, buf, 10);
+	printf("Read: %s\n", buf);
+	close(fd);
 }
--- ClassExercises/sysfs_chardev/syschar.c
+++ ClassExercises/sysfs_chardev/syschar.c
@@ -17,7 +17,7 @@
 #define DEVNAME "demo"
 
 static int major;
-atomic_t  device_opened;
+atomic_t device_opened;
 static unsigned buf_size = MAX_BUF_SIZE;
 static unsigned current_usage;
 
@@ -28,297 +28,320 @@ static pid_t cpid;
 
 static unsigned total_bytes_read;
 static unsigned total_bytes_written;
-char* device_buffer = NULL;
+char *device_buffer = NULL;
 
 struct mutex dev_mutex;
 
 struct kobj_list {
-  struct kobject *kobj;
-  unsigned current_buf_usage;
-  unsigned total_bytes_read;
-  unsigned total_bytes_written;
-  struct list_head list;
+	struct kobject *kobj;
+	unsigned current_buf_usage;
+	unsigned total_bytes_read;
+	unsigned total_bytes_written;
+	struct list_head list;
 };
 
 struct kobj_list demodev_kobj_list;
 struct kobj_list *tmp;
 struct list_head *pos, *q;
 
-static struct kobj_list* getDemodevKobjProcess(struct kobject *kobj){
-  list_for_each_safe(pos, q, &demodev_kobj_list.list){
-    tmp = list_entry(pos, struct kobj_list, list);
-    if((tmp->kobj) == kobj){
-      return tmp;
-    }
-  }
-  return NULL;
+static struct kobj_list *getDemodevKobjProcess(struct kobject *kobj)
+{
+	list_for_each_safe(pos, q, &demodev_kobj_list.list) {
+		tmp = list_entry(pos, struct kobj_list, list);
+		if ((tmp->kobj) == kobj) {
+			return tmp;
+		}
+	}
+	return NULL;
 }
 
-
-static ssize_t subdir_current_usage_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-  tmp = getDemodevKobjProcess(kobj);
-  if(tmp)
-    return sprintf(buf, "%d\n", tmp->current_buf_usage);
-  return sprintf(buf, "%d\n", -1);
+static ssize_t subdir_current_usage_show(struct kobject *kobj,
+					 struct kobj_attribute *attr, char *buf)
+{
+	tmp = getDemodevKobjProcess(kobj);
+	if (tmp)
+		return sprintf(buf, "%d\n", tmp->current_buf_usage);
+	return sprintf(buf, "%d\n", -1);
 }
-static struct kobj_attribute subdir_current_usage_attribute = __ATTR(current_usage, 0444, subdir_current_usage_show, NULL);
 
-static ssize_t subdir_total_bytes_read_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-  tmp = getDemodevKobjProcess(kobj); 
-  if(tmp)
-    return sprintf(buf, "%d\n", tmp->total_bytes_read);
-  return sprintf(buf, "%d\n", -1);
-}
-static struct kobj_attribute subdir_total_bytes_read_attribute = __ATTR(total_bytes_read, 0444, subdir_total_bytes_read_show, NULL);
+static struct kobj_attribute subdir_current_usage_attribute =
+__ATTR(current_usage, 0444, subdir_current_usage_show, NULL);
 
-static ssize_t subdir_total_bytes_written_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-  tmp = getDemodevKobjProcess(kobj);
-  if(tmp)
-    return sprintf(buf, "%d\n", tmp->total_bytes_written);
-  return sprintf(buf, "%d\n", -1);
+static ssize_t subdir_total_bytes_read_show(struct kobject *kobj,
+					    struct kobj_attribute *attr,
+					    char *buf)
+{
+	tmp = getDemodevKobjProcess(kobj);
+	if (tmp)
+		return sprintf(buf, "%d\n", tmp->total_bytes_read);
+	return sprintf(buf, "%d\n", -1);
 }
-static struct kobj_attribute subdir_total_bytes_written_attribute = __ATTR(total_bytes_written, 0444, subdir_total_bytes_written_show, NULL);
+
+static struct kobj_attribute subdir_total_bytes_read_attribute =
+__ATTR(total_bytes_read, 0444, subdir_total_bytes_read_show, NULL);
+
+static ssize_t subdir_total_bytes_written_show(struct kobject *kobj,
+					       struct kobj_attribute *attr,
+					       char *buf)
+{
+	tmp = getDemodevKobjProcess(kobj);
+	if (tmp)
+		return sprintf(buf, "%d\n", tmp->total_bytes_written);
+	return sprintf(buf, "%d\n", -1);
+}
+
+static struct kobj_attribute subdir_total_bytes_written_attribute =
+__ATTR(total_bytes_written, 0444, subdir_total_bytes_written_show, NULL);
 
 static struct attribute *subdir_attrs[] = {
-  &subdir_current_usage_attribute.attr,
-  &subdir_total_bytes_read_attribute.attr,
-  &subdir_total_bytes_written_attribute.attr,
-  NULL
-};
-static struct attribute_group subdir_attr_group = {
-  .attrs = subdir_attrs,
+	&subdir_current_usage_attribute.attr,
+	&subdir_total_bytes_read_attribute.attr,
+	&subdir_total_bytes_written_attribute.attr,
+	NULL
 };
 
+static struct attribute_group subdir_attr_group = {
+	.attrs = subdir_attrs,
+};
 
 static int demo_open(struct inode *inode, struct file *file)
 {
-  int ret;
-  atomic_inc(&device_opened);
-  try_module_get(THIS_MODULE);
-  printk(KERN_INFO "Device opened successfully\n");
-  cpid = current->pid;
-  snprintf(pid, 10, "%d", (int)cpid);
-  subdir_kobj = kobject_create_and_add(pid, demodev_kobj);
-  BUG_ON(!subdir_kobj);
+	int ret;
+	atomic_inc(&device_opened);
+	try_module_get(THIS_MODULE);
+	printk(KERN_INFO "Device opened successfully\n");
+	cpid = current->pid;
+	snprintf(pid, 10, "%d", (int)cpid);
+	subdir_kobj = kobject_create_and_add(pid, demodev_kobj);
+	BUG_ON(!subdir_kobj);
 
-  tmp = (struct kobj_list*)kmalloc(sizeof(struct kobj_list), GFP_KERNEL);
-  tmp->kobj = subdir_kobj;
-  tmp->current_buf_usage = 0;
-  tmp->total_bytes_read = 0;
-  tmp->total_bytes_written = 0;
-  list_add(&(tmp->list), &(demodev_kobj_list.list));
+	tmp = (struct kobj_list *)kmalloc(sizeof(struct kobj_list), GFP_KERNEL);
+	tmp->kobj = subdir_kobj;
+	tmp->current_buf_usage = 0;
+	tmp->total_bytes_read = 0;
+	tmp->total_bytes_written = 0;
+	list_add(&(tmp->list), &(demodev_kobj_list.list));
 
-  ret = sysfs_create_group (subdir_kobj, &subdir_attr_group);
-  if(unlikely(ret)){
-      printk(KERN_INFO "demodev: can't create sysfs\n");
-      BUG_ON(1);
-  }
-  return 0;
+	ret = sysfs_create_group(subdir_kobj, &subdir_attr_group);
+	if (unlikely(ret)) {
+		printk(KERN_INFO "demodev: can't create sysfs\n");
+		BUG_ON(1);
+	}
+	return 0;
 }
-
 
 static int demo_release(struct inode *inode, struct file *file)
 {
-        atomic_dec(&device_opened);
-        module_put(THIS_MODULE);
-        cpid = current->pid;
-        snprintf(pid, 10, "%d", (int)cpid);
-        list_for_each_safe(pos, q, &demodev_kobj_list.list){
-          tmp = list_entry(pos, struct kobj_list, list);
-          if(!strcmp(tmp->kobj->name, pid)){
-                subdir_kobj = tmp->kobj;
-                list_del(pos);
-                kfree(tmp);
-              }
-        }
-        sysfs_remove_group(subdir_kobj, &subdir_attr_group);
-        kobject_del(subdir_kobj);
-        printk(KERN_INFO "Device closed successfully\n");
-        return 0;
+	atomic_dec(&device_opened);
+	module_put(THIS_MODULE);
+	cpid = current->pid;
+	snprintf(pid, 10, "%d", (int)cpid);
+	list_for_each_safe(pos, q, &demodev_kobj_list.list) {
+		tmp = list_entry(pos, struct kobj_list, list);
+		if (!strcmp(tmp->kobj->name, pid)) {
+			subdir_kobj = tmp->kobj;
+			list_del(pos);
+			kfree(tmp);
+		}
+	}
+	sysfs_remove_group(subdir_kobj, &subdir_attr_group);
+	kobject_del(subdir_kobj);
+	printk(KERN_INFO "Device closed successfully\n");
+	return 0;
 }
 
-static struct kobj_list* getDemodevKobjProcessByName(void){
-  cpid = current->pid;
-  snprintf(pid, 10, "%d", (int)cpid);
-  list_for_each_safe(pos, q, &demodev_kobj_list.list){
-    tmp = list_entry(pos, struct kobj_list, list);
-    if(!strcmp(tmp->kobj->name, pid)){
-      return tmp;
-    }
-  }
-  return NULL;
+static struct kobj_list *getDemodevKobjProcessByName(void)
+{
+	cpid = current->pid;
+	snprintf(pid, 10, "%d", (int)cpid);
+	list_for_each_safe(pos, q, &demodev_kobj_list.list) {
+		tmp = list_entry(pos, struct kobj_list, list);
+		if (!strcmp(tmp->kobj->name, pid)) {
+			return tmp;
+		}
+	}
+	return NULL;
 }
-
-
 
 static ssize_t demo_read(struct file *filp,
-                           char *buffer,
-                           size_t length,
-                           loff_t * offset)
+			 char *buffer, size_t length, loff_t * offset)
 {
-  if(length > current_usage)
-    length = current_usage;
-  mutex_lock_interruptible(&dev_mutex);
-  printk(KERN_INFO "Read!\n");
-  copy_to_user(buffer, device_buffer, length); 
-  mutex_unlock(&dev_mutex);
-  current_usage = 0;
-  /*printk(KERN_INFO "Sorry, this operation isn't supported.\n");*/
-  *offset += length;
-  total_bytes_read += length;
+	if (length > current_usage)
+		length = current_usage;
+	mutex_lock_interruptible(&dev_mutex);
+	printk(KERN_INFO "Read!\n");
+	copy_to_user(buffer, device_buffer, length);
+	mutex_unlock(&dev_mutex);
+	current_usage = 0;
+	/*printk(KERN_INFO "Sorry, this operation isn't supported.\n"); */
+	*offset += length;
+	total_bytes_read += length;
 
-  tmp = getDemodevKobjProcessByName();
-  tmp->current_buf_usage = 0;
-  tmp->total_bytes_read += length;
+	tmp = getDemodevKobjProcessByName();
+	tmp->current_buf_usage = 0;
+	tmp->total_bytes_read += length;
 
-  return length;
+	return length;
 }
 
 static ssize_t
 demo_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
-  if(len > buf_size)
-    len = buf_size;
-  mutex_lock_interruptible(&dev_mutex);
-  printk(KERN_INFO "Write!\n");
-  copy_from_user(device_buffer, buff, len);
-  mutex_unlock(&dev_mutex);
-  current_usage = len;
-  /*printk(KERN_INFO "Sorry, this operation isn't supported.\n");*/
-  *off += len;
-  total_bytes_written += len;
+	if (len > buf_size)
+		len = buf_size;
+	mutex_lock_interruptible(&dev_mutex);
+	printk(KERN_INFO "Write!\n");
+	copy_from_user(device_buffer, buff, len);
+	mutex_unlock(&dev_mutex);
+	current_usage = len;
+	/*printk(KERN_INFO "Sorry, this operation isn't supported.\n"); */
+	*off += len;
+	total_bytes_written += len;
 
-  tmp = getDemodevKobjProcessByName();
-  tmp->current_buf_usage = len;
-  tmp->total_bytes_written += len;
+	tmp = getDemodevKobjProcessByName();
+	tmp->current_buf_usage = len;
+	tmp->total_bytes_written += len;
 
-  return len;
+	return len;
 }
 
 static ssize_t demodev_buf_size_show(struct kobject *kobj,
-                                  struct kobj_attribute *attr, char *buf)
+				     struct kobj_attribute *attr, char *buf)
 {
-        return sprintf(buf, "%u\n", buf_size);
+	return sprintf(buf, "%u\n", buf_size);
 }
 
 static ssize_t demodev_buf_size_set(struct kobject *kobj,
-                                   struct kobj_attribute *attr,
-                                   const char *buf, size_t count)
+				    struct kobj_attribute *attr,
+				    const char *buf, size_t count)
 {
-  int err;
-  unsigned long mode;
+	int err;
+	unsigned long mode;
 
-  if(current_usage){
-      printk(KERN_INFO "Can not change size while buf being used\n");
-      return -EINVAL;
-  }
-  err = kstrtoul(buf, 10, &mode);
-  if (err || mode < 0 || mode > MAX_BUF_SIZE )
-      return -EINVAL;
+	if (current_usage) {
+		printk(KERN_INFO "Can not change size while buf being used\n");
+		return -EINVAL;
+	}
+	err = kstrtoul(buf, 10, &mode);
+	if (err || mode < 0 || mode > MAX_BUF_SIZE)
+		return -EINVAL;
 
-  buf_size = mode;
-  kfree(device_buffer);
-  device_buffer = (char*)kmalloc(buf_size, GFP_KERNEL);
-  return count;
+	buf_size = mode;
+	kfree(device_buffer);
+	device_buffer = (char *)kmalloc(buf_size, GFP_KERNEL);
+	return count;
 }
-static struct kobj_attribute demodev_buf_size_attribute = __ATTR(buffer_size,0644,demodev_buf_size_show, demodev_buf_size_set);
+
+static struct kobj_attribute demodev_buf_size_attribute =
+__ATTR(buffer_size, 0644, demodev_buf_size_show, demodev_buf_size_set);
 
 static ssize_t demodev_usage_count_show(struct kobject *kobj,
-                                  struct kobj_attribute *attr, char *buf)
+					struct kobj_attribute *attr, char *buf)
 {
-        return sprintf(buf, "%d\n", atomic_read(&device_opened));
+	return sprintf(buf, "%d\n", atomic_read(&device_opened));
 }
-static struct kobj_attribute demodev_usage_count_attribute = __ATTR(usage_count, 0444,demodev_usage_count_show, NULL);
 
+static struct kobj_attribute demodev_usage_count_attribute =
+__ATTR(usage_count, 0444, demodev_usage_count_show, NULL);
 
-static ssize_t demodev_current_usage_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-  return sprintf(buf, "%d\n", current_usage);
+static ssize_t demodev_current_usage_show(struct kobject *kobj,
+					  struct kobj_attribute *attr,
+					  char *buf)
+{
+	return sprintf(buf, "%d\n", current_usage);
 }
-static struct kobj_attribute demodev_current_usage_attribute = __ATTR(current_usage, 0444, demodev_current_usage_show, NULL);
 
+static struct kobj_attribute demodev_current_usage_attribute =
+__ATTR(current_usage, 0444, demodev_current_usage_show, NULL);
 
-
-static ssize_t demodev_total_bytes_read_show(struct kobject *kobj, struct kobj_attribute *attr, char * buf){
-  return sprintf(buf, "%d\n", total_bytes_read);
+static ssize_t demodev_total_bytes_read_show(struct kobject *kobj,
+					     struct kobj_attribute *attr,
+					     char *buf)
+{
+	return sprintf(buf, "%d\n", total_bytes_read);
 }
-static struct kobj_attribute demodev_total_bytes_read_attribute = __ATTR(total_bytes_read, 0444, demodev_total_bytes_read_show, NULL);
 
+static struct kobj_attribute demodev_total_bytes_read_attribute =
+__ATTR(total_bytes_read, 0444, demodev_total_bytes_read_show, NULL);
 
-static ssize_t demodev_total_bytes_written_show(struct kobject *kobj, struct kobj_attribute *attr, char* buf){
-  return sprintf(buf, "%d\n", total_bytes_written);
+static ssize_t demodev_total_bytes_written_show(struct kobject *kobj,
+						struct kobj_attribute *attr,
+						char *buf)
+{
+	return sprintf(buf, "%d\n", total_bytes_written);
 }
-static struct kobj_attribute demodev_total_bytes_written_attribute = __ATTR(total_bytes_written, 0444, demodev_total_bytes_written_show, NULL);
 
+static struct kobj_attribute demodev_total_bytes_written_attribute =
+__ATTR(total_bytes_written, 0444, demodev_total_bytes_written_show, NULL);
 
 static struct attribute *demodev_attrs[] = {
-        &demodev_buf_size_attribute.attr,
-        &demodev_usage_count_attribute.attr,
-        &demodev_current_usage_attribute.attr,
-        &demodev_total_bytes_read_attribute.attr,
-        &demodev_total_bytes_written_attribute.attr,
-        NULL,
+	&demodev_buf_size_attribute.attr,
+	&demodev_usage_count_attribute.attr,
+	&demodev_current_usage_attribute.attr,
+	&demodev_total_bytes_read_attribute.attr,
+	&demodev_total_bytes_written_attribute.attr,
+	NULL,
 };
+
 static struct attribute_group demodev_attr_group = {
-        .attrs = demodev_attrs,
-        /*.name = "demodev",*/
+	.attrs = demodev_attrs,
+	/*.name = "demodev", */
 };
 
 static long demo_ioctl(struct file *file,
-                 unsigned int ioctl_num,
-                 unsigned long arg)
-
+		       unsigned int ioctl_num, unsigned long arg)
 {
-   printk(KERN_INFO "Sorry, this operation isn't supported.\n");
-   return -EINVAL;
+	printk(KERN_INFO "Sorry, this operation isn't supported.\n");
+	return -EINVAL;
 }
+
 static struct file_operations fops = {
-        .read = demo_read,
-        .write = demo_write,
-        .open = demo_open,
-        .release = demo_release,
-        .unlocked_ioctl = demo_ioctl,
+	.read = demo_read,
+	.write = demo_write,
+	.open = demo_open,
+	.release = demo_release,
+	.unlocked_ioctl = demo_ioctl,
 };
-
-
-
 
 int init_module(void)
 {
-  int ret;
-  printk(KERN_INFO "Hello kernel\n");
-  major = register_chrdev(0, DEVNAME, &fops);
-  if (major < 0) {      
-    printk(KERN_ALERT "Registering char device failed with %d\n", major);   
-    return major;
-  }                 
+	int ret;
+	printk(KERN_INFO "Hello kernel\n");
+	major = register_chrdev(0, DEVNAME, &fops);
+	if (major < 0) {
+		printk(KERN_ALERT "Registering char device failed with %d\n",
+		       major);
+		return major;
+	}
 
-  printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);                                                              
-  printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVNAME, major);  
-  atomic_set(&device_opened, 0);
+	printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVNAME, major);
+	atomic_set(&device_opened, 0);
 
-  /*sysfs creation*/
-  demodev_kobj = kobject_create_and_add("demodev", kernel_kobj);
-   /*demodev_kobj = kernel_kobj;*/
-   BUG_ON(!demodev_kobj);
-   ret = sysfs_create_group (demodev_kobj, &demodev_attr_group);
-   if(unlikely(ret))
-          printk(KERN_INFO "demodev: can't create sysfs\n");
- 
-   INIT_LIST_HEAD(&demodev_kobj_list.list);
-  mutex_init(&dev_mutex);
-  device_buffer = (char*)kmalloc(MAX_BUF_SIZE, GFP_KERNEL);
-  memset(device_buffer, 0, MAX_BUF_SIZE);
+	/*sysfs creation */
+	demodev_kobj = kobject_create_and_add("demodev", kernel_kobj);
+	/*demodev_kobj = kernel_kobj; */
+	BUG_ON(!demodev_kobj);
+	ret = sysfs_create_group(demodev_kobj, &demodev_attr_group);
+	if (unlikely(ret))
+		printk(KERN_INFO "demodev: can't create sysfs\n");
+
+	INIT_LIST_HEAD(&demodev_kobj_list.list);
+	mutex_init(&dev_mutex);
+	device_buffer = (char *)kmalloc(MAX_BUF_SIZE, GFP_KERNEL);
+	memset(device_buffer, 0, MAX_BUF_SIZE);
 	return 0;
 }
 
 void cleanup_module(void)
 {
-  unregister_chrdev(major, DEVNAME);
-  sysfs_remove_group (demodev_kobj, &demodev_attr_group);
-  kobject_del(demodev_kobj);
-  kfree(device_buffer);
-        printk(KERN_INFO "Goodbye kernel\n");
+	unregister_chrdev(major, DEVNAME);
+	sysfs_remove_group(demodev_kobj, &demodev_attr_group);
+	kobject_del(demodev_kobj);
+	kfree(device_buffer);
+	printk(KERN_INFO "Goodbye kernel\n");
 }
+
 MODULE_AUTHOR("deba@cse.iitk.ac.in");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Demo modules");
